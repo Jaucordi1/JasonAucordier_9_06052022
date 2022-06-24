5ea506698d4e15760caa55b2ba7b48b907f556dd
Jaucordi <jaucordi@gmail.com>
2022-06-24 18:05:29 +0200

    Clean & remove navigation when submit form before updating new bill in database
    Signed-off-by: Jaucordi <jaucordi@gmail.com>

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -50,7 +50,6 @@ export default class NewBill {
   }
   handleSubmit = e => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
@@ -66,7 +65,7 @@ export default class NewBill {
       status: 'pending'
     }
     this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
+    // this.onNavigate(ROUTES_PATH['Bills'])
   }
 
   isFileAllowed = (file) => {
