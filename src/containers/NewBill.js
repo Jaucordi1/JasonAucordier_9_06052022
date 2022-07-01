7da76500fdc7ae842bdd2da042820f4f769528fa
Jaucordi <jaucordi@gmail.com>
2022-07-01 16:58:02 +0200

    Simplify input file change handler
    Signed-off-by: Jaucordi <jaucordi@gmail.com>

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,7 +18,7 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.fileInput.files[0]
+    const file = e.target.files[0];
     if (!this.isFileAllowed(file)) {
       this.fileInput.setCustomValidity("Seul les images jp(e)g et png sont prises en charges.")
       return
@@ -26,8 +26,6 @@ export default class NewBill {
       this.fileInput.setCustomValidity("")
     }
 
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
@@ -45,7 +43,7 @@ export default class NewBill {
         console.log(fileUrl)
         this.billId = key
         this.fileUrl = fileUrl
-        this.fileName = fileName
+        this.fileName = file.name
       }).catch(error => console.error(error))
   }
   handleSubmit = e => {
